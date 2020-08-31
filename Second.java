78bf25abc88aa78797cf1ec3cee336d39c5bce6e
baale <baalelk@gmail.com>
2020-08-31 15:28:06 +0530

    this is first commit

---
+++ Second.java
@@ -0,0 +1,8 @@
+
+Class Second{
+
+	public static void main(String[] args){
+	
+	System.out.println("this is second program");
+	}
+}
---
+++ Third.java
@@ -0,0 +1,10 @@
+
+Class third{
+
+	public static void main(String[] args)
+	{
+		System.out.println("this is third Class");
+	}
+
+
+}
